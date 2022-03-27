e5bdb13622da312a599ba2370de0382047d23cf3
Gururaj-Gg <79778518+Gururaj-Gg@users.noreply.github.com>
2022-03-27 15:00:55 +0530

    Update bubl.c

--- bubl.c
+++ bubl.c
@@ -26,7 +26,7 @@ scanf("%d",&n);
 printf("enter the element:");
 for(i=0;i<n;i++)
 scanf("%d",&a[i]);
-buble_sort();
+buble_sort(a);
 printf("sorted data is:\n");
 for(i=0;i<n;i++)
 printf(" %d",a[i]);
